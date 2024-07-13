0f75a986f7be62fc15fff10331c7693098eb7165
GeoKD <gioman5817@gmail.com>
2024-07-13 18:35:38 +0300

    Ready to write some features!

--- src/main/java/KedelidzeKrutyakov/application/service/ApplicationService.java
+++ src/main/java/KedelidzeKrutyakov/application/service/ApplicationService.java
@@ -1,10 +1,7 @@
 package KedelidzeKrutyakov.application.service;
 
-import KedelidzeKrutyakov.application.api.DTO.LoanApplicationRequestDTO;
-import KedelidzeKrutyakov.application.api.DTO.LoanOfferDTO;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class ApplicationService {
