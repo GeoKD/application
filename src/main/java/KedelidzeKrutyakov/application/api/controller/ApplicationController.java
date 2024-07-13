14442a850de1931cb99b0aae4e9255d2d6f6041a
GeoKD <gioman5817@gmail.com>
2024-07-13 18:53:43 +0300

    Working. Need to write prescoring

--- src/main/java/KedelidzeKrutyakov/application/api/controller/ApplicationController.java
+++ src/main/java/KedelidzeKrutyakov/application/api/controller/ApplicationController.java
@@ -1,9 +1,15 @@
 package KedelidzeKrutyakov.application.api.controller;
 
+import KedelidzeKrutyakov.application.api.DTO.LoanApplicationRequestDTO;
+import KedelidzeKrutyakov.application.api.DTO.LoanOfferDTO;
 import KedelidzeKrutyakov.application.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/application")
@@ -11,5 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationController {
 
     private final ApplicationService applicationService;
+
+    @PostMapping
+    public List<LoanOfferDTO> getLoanOffers(
+            @RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO ) {
+        return applicationService.getLoanOffers(loanApplicationRequestDTO);
+    }
 
 }
--- src/main/java/KedelidzeKrutyakov/application/service/ApplicationService.java
+++ src/main/java/KedelidzeKrutyakov/application/service/ApplicationService.java
@@ -1,9 +1,21 @@
 package KedelidzeKrutyakov.application.service;
 
+import KedelidzeKrutyakov.application.api.DTO.LoanApplicationRequestDTO;
+import KedelidzeKrutyakov.application.api.DTO.LoanOfferDTO;
+import KedelidzeKrutyakov.application.api.feign.DealApi;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class ApplicationService {
 
+    private final DealApi dealApi;
+
+    public List<LoanOfferDTO> getLoanOffers(LoanApplicationRequestDTO loanApplicationRequestDTO) {
+        return dealApi.getLoanOffers(loanApplicationRequestDTO);
+    }
 }
